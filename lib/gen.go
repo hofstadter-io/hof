86f6e3f769c9b3ec62b4f78ebf6720eafb139d5f
Tony Worm <tony@hofstadter.io>
2020-03-25 21:51:26 -0600

    start generating files, also format. reach parity with pure cue version

--- lib/gen.go
+++ lib/gen.go
@@ -1,11 +1,21 @@
 package lib
 
 import (
+	// "bytes"
 	"fmt"
+	// "go/format"
+	// "io/ioutil"
 	"os"
+	// "path"
+	"strings"
+	"text/template"
+
+	// "github.com/kr/pretty"
 
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/load"
+
+	"github.com/hofstadter-io/hof/lib/util"
 )
 
 func Gen(entrypoints, expressions []string, mode string) (string, error) {
@@ -13,7 +23,7 @@ func Gen(entrypoints, expressions []string, mode string) (string, error) {
 
 	var rt cue.Runtime
 
-	// out := make(map[string]interface{})
+	out := make(map[string]interface{})
 
 	bis := load.Instances([]string{}, nil)
 	for i, bi := range bis {
@@ -54,33 +64,49 @@ func Gen(entrypoints, expressions []string, mode string) (string, error) {
 
 			fmt.Printf(" - %v %b\n\n%#+v\n\n\n", label, ev.IsConcrete(), ev)
 
-			vi := 0
+			/*
+				vi := 0
 
-			value.Walk(func(val cue.Value) bool {
-				// l, _ := val.Label()
-				// k := val.Kind()
-				// fmt.Println(vi, l, k)
-				vi += 1
+				value.Walk(func(val cue.Value) bool {
+					// l, _ := val.Label()
+					// k := val.Kind()
+					// fmt.Println(vi, l, k)
+					vi += 1
 
-				return true
-			}, nil)
+					return true
+				}, nil)
 
-			fmt.Println("VI: ", vi)
+				fmt.Println("VI: ", vi)
+			*/
 
 			// Put anything starting with Gen into
 			// our out map
 
-			/*
-				if strings.HasPrefix(label, "Gen") {
-					var obj map[string]interface{}
-					err = value.Decode(&obj)
-					if err != nil {
-						fmt.Println(err)
-						continue
-					}
-					out[label] = obj
+			if strings.HasPrefix(label, "Gen") {
+				var gen map[string]interface{}
+				err = value.Decode(&gen)
+				if err != nil {
+					fmt.Println(err)
+					continue
 				}
-			*/
+
+				IN, ok := gen["In"].(map[string]interface{})
+				OUT, ok := gen["Out"].([]interface{})
+				if !ok {
+					return "", fmt.Errorf("Generator: %q is missing 'Out' field.", label)
+				}
+
+				for _, O := range OUT {
+					o := O.(map[string]interface{})
+					renderFile(IN, o)
+				}
+
+				// obj := OUT
+				// fmt.Printf("%# v\n\n", pretty.Formatter(obj))
+				out[label] = OUT
+
+				break
+			}
 		}
 	}
 
@@ -101,5 +127,75 @@ func Gen(entrypoints, expressions []string, mode string) (string, error) {
 		// TODO see if we can parse and introspect *_tool.cue files
 	*/
 
-	return "", nil
+	stdout, err := util.Exec([]string{"goimports", "-w", "-l", "."})
+	if err != nil {
+		return "", err
+	}
+
+	return stdout, nil
+}
+
+func renderFile(IN, file map[string]interface{}) error {
+	// Look for input on the file
+	in, ok := file["In"].(map[string]interface{})
+
+	// If not there, use the global IN
+	if !ok {
+		file["In"] = IN
+
+	} else {
+
+		// Else, 'IN' has key and 'in' does not, add it
+		for key, val := range IN {
+			if _, ok := in[key]; !ok {
+				in[key] = val
+			}
+		}
+	}
+
+	fmt.Println(file["Filename"])
+
+	fn := file["Filename"].(string)
+	tp := file["Template"].(string)
+	// alt := file["Alt"].(bool)
+
+	t := template.Must(template.New(fn).Parse(tp))
+
+	f, err := os.Create(fn)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	err = t.Execute(f, in)
+	if err != nil {
+		return err
+	}
+
+	/*
+		var b bytes.Buffer
+		var err error
+
+		err = t.Execute(&b, in)
+		if err != nil {
+			return err
+		}
+
+		fmtd, err := format.Source(b.Bytes())
+		if err != nil {
+			return err
+		}
+
+		err = os.MkdirAll(path.Dir(fn), 0755)
+		if err != nil {
+			return err
+		}
+
+		err = ioutil.WriteFile(fn, fmtd, 0644)
+		if err != nil {
+			return err
+		}
+	*/
+
+	return nil
 }
