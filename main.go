97b5c50dbc16851fd59688b9f1d72bf6ebaf5134
Tony Worm <tony@hofstadter.io>
2020-03-28 00:49:40 -0600

    remove pprof code that got diffed in permenantly after dep update

--- main.go
+++ main.go
@@ -4,21 +4,10 @@ import (
 	"fmt"
 	"os"
 
-	"log"
-	"runtime/pprof"
-
 	"github.com/hofstadter-io/hof/commands"
 )
 
 func main() {
-
-	// TODO, turn this into a flag and run if enabled, in root command persistent-pre-run, the flag should be a filename so we know where to write it
-	f, err := os.Create("hof-cpu.prof")
-	if err != nil {
-		log.Fatal(err)
-	}
-	pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()
 
 	if err := commands.RootCmd.Execute(); err != nil {
 		fmt.Println(err)
