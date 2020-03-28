fe9995d7c49ad100b7405a70ca3a3d10d6b42113
Tony Worm <tony@hofstadter.io>
2020-03-28 12:43:52 -0600

    improve cue loading and decoding into structs

--- lib/gen/loadcue.go
+++ lib/gen/loadcue.go
@@ -20,14 +20,13 @@ func (G *Generator) LoadCue() (error) {
 	}
 
 	// finalize load timing stats
-	decodeTime := time.Now()
-	G.Stats.CueLoadingTime = decodeTime.Sub(start)
+	cueDecodeTime := time.Now()
+	G.Stats.CueLoadingTime = cueDecodeTime.Sub(start)
 
-	// Get the Generator Input (if it has one)
-	In, ok := gen["In"].(map[string]interface{})
-	if ok {
-		G.In = In
-	}
+	return G.decodeGenerator(gen)
+}
+
+func (G *Generator) decodeGenerator(gen map[string]interface{}) (error) {
 
 	// Get Out, or the files we want to render, required
 	Out, ok := gen["Out"].([]interface{})
@@ -35,94 +34,32 @@ func (G *Generator) LoadCue() (error) {
 		return fmt.Errorf("Generator: %q is missing 'Out' field.", G.Name)
 	}
 
+	// Get the Generator Input (if it has one)
+	In, ok := gen["In"].(map[string]interface{})
+	if ok {
+		G.In = In
+	}
+
+	G.PackageName, _  = gen["PackageName"].(string)
+
+	// In cue code
+	G.NamedTemplates, _ = gen["NamedTemplates"].(map[string]string)
+	G.NamedPartials,  _ = gen["NamedPartials"].(map[string]string)
+	G.StaticFiles,    _ = gen["StaticFiles"].(map[string]string)
+
+	// Eventually loaded from disk
+	G.TemplatesDir, _ = gen["TemplatesDir"].(string)
+	G.PartialsDir, _  = gen["PartialsDir"].(string)
+	G.StaticGlobs, _  = gen["StaticGlobs"].([]string)
+
+	// TODO, load subgenerators
+
+	// Decode generator files
 	// Turn G.Out elements into G.Files
 	for i, O := range Out {
 		file := O.(map[string]interface{})
 
-		// Is this output missing a filename? then skip it
-		if _, ok := file["Filepath"]; !ok {
-			mockname := fmt.Sprintf("noname-%d", i)
-			F := &File {
-				FileStats: FileStats{
-					IsSkipped: 1,
-				},
-				FinalContent: []byte(mockname),
-			}
-
-			G.Files[mockname] = F
-			continue
-		}
-
-		// TODO, better checking and/or decode directly into golang structs
-		// But... they do all have defaults in the schema, so we will probably be OK
-		// EXCEPT, spelling errors...
-
-		// Build up the files "In" value
-		in, ok := file["In"].(map[string]interface{})
-		if !ok {
-			in = G.In
-		} else {
-			// Else, 'IN' has key and 'in' does not, add it
-			for key, val := range G.In {
-				if _, ok := in[key]; !ok {
-					// fmt.Println("checking In, filling", key)
-					in[key] = val
-				}
-			}
-		}
-
-		// Meta information
-		fn := file["Filepath"].(string)
-		tc := file["Template"].(string)
-		tn := file["TemplateName"].(string)
-		ts := file["TemplateSystem"].(string)
-
-		// deleimters
-		ALT  := file["AltDelims"].(bool)
-		SWAP := file["SwapDelims"].(bool)
-
-		LHS2_D := file["LHS2_D"].(string)
-		RHS2_D := file["RHS2_D"].(string)
-		LHS3_D := file["LHS3_D"].(string)
-		RHS3_D := file["RHS3_D"].(string)
-
-		LHS2_S := file["LHS2_S"].(string)
-		RHS2_S := file["RHS2_S"].(string)
-		LHS3_S := file["LHS3_S"].(string)
-		RHS3_S := file["RHS3_S"].(string)
-
-		LHS2_T := file["LHS2_T"].(string)
-		RHS2_T := file["RHS2_T"].(string)
-		LHS3_T := file["LHS3_T"].(string)
-		RHS3_T := file["RHS3_T"].(string)
-
-		// Store the file in the generator
-		F := &File {
-			In: in,
-			Filepath: fn,
-			Template: tc,
-			TemplateName: tn,
-			TemplateSystem: ts,
-
-			AltDelims: ALT,
-			SwapDelims: SWAP,
-
-			LHS2_D: LHS2_D,
-			RHS2_D: RHS2_D,
-			LHS3_D: LHS3_D,
-			RHS3_D: RHS3_D,
-
-			LHS2_S: LHS2_S,
-			RHS2_S: RHS2_S,
-			LHS3_S: LHS3_S,
-			RHS3_S: RHS3_S,
-
-			LHS2_T: LHS2_T,
-			RHS2_T: RHS2_T,
-			LHS3_T: LHS3_T,
-			RHS3_T: RHS3_T,
-
-		}
+		F := G.decodeFile(i, file)
 
 		G.Files[F.Filepath] = F
 
@@ -135,3 +72,67 @@ func (G *Generator) LoadCue() (error) {
 	return nil
 }
 
+func (G *Generator) decodeFile(i int, file map[string]interface{}) *File {
+
+	// Is this output missing a filename? then skip it
+	if _, ok := file["Filepath"]; !ok {
+		mockname := fmt.Sprintf("noname-%d", i)
+		F := &File {
+			FileStats: FileStats{
+				IsSkipped: 1,
+			},
+			FinalContent: []byte(mockname),
+		}
+		return F
+
+	}
+
+	// TODO, better checking and/or decode directly into golang structs
+	// But... they do all have defaults in the schema, so we will probably be OK
+	// EXCEPT, spelling errors...
+
+	// Build up the files "In" value
+	in, ok := file["In"].(map[string]interface{})
+	if !ok {
+		in = G.In
+	} else {
+		// Else, 'IN' has key and 'in' does not, add it
+		for key, val := range G.In {
+			if _, ok := in[key]; !ok {
+				// fmt.Println("checking In, filling", key)
+				in[key] = val
+			}
+		}
+	}
+
+	F := &File {
+		In: in,
+	}
+
+	// Meta information
+	F.Filepath = file["Filepath"].(string)
+	F.Template = file["Template"].(string)
+	F.TemplateName = file["TemplateName"].(string)
+	F.TemplateSystem = file["TemplateSystem"].(string)
+
+	// deleimters
+	F.AltDelims  = file["AltDelims"].(bool)
+	F.SwapDelims = file["SwapDelims"].(bool)
+
+	F.LHS2_D = file["LHS2_D"].(string)
+	F.RHS2_D = file["RHS2_D"].(string)
+	F.LHS3_D = file["LHS3_D"].(string)
+	F.RHS3_D = file["RHS3_D"].(string)
+
+	F.LHS2_S = file["LHS2_S"].(string)
+	F.RHS2_S = file["RHS2_S"].(string)
+	F.LHS3_S = file["LHS3_S"].(string)
+	F.RHS3_S = file["RHS3_S"].(string)
+
+	F.LHS2_T = file["LHS2_T"].(string)
+	F.RHS2_T = file["RHS2_T"].(string)
+	F.LHS3_T = file["LHS3_T"].(string)
+	F.RHS3_T = file["RHS3_T"].(string)
+
+	return F
+}
