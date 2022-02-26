53abc59008ed01b1b3a1ad27aeff5d71d908f67a
Tony Worm <tony@hofstadter.io>
2022-02-25 22:35:40 -0500

    remove unused middlewares

--- flow/middleware/defaults.go
+++ flow/middleware/defaults.go
@@ -3,17 +3,17 @@ package middleware
 import ( 
 	"github.com/hofstadter-io/hof/cmd/hof/flags"
   hofcontext "github.com/hofstadter-io/hof/flow/context"
-	"github.com/hofstadter-io/hof/flow/middleware/dummy"
+
 	"github.com/hofstadter-io/hof/flow/middleware/info"
 	"github.com/hofstadter-io/hof/flow/middleware/sync"
 )
 
 func UseDefaults(ctx *hofcontext.Context, opts *flags.RootPflagpole, popts *flags.FlowFlagpole) {
-  ctx.Use(dummy.NewDummy(opts, popts))
+  // ctx.Use(dummy.NewDummy(opts, popts))
   ctx.Use(info.NewPrint(opts, popts))
   ctx.Use(info.NewProgress(opts, popts))
-  ctx.Use(info.NewBookkeeping(info.BookkeepingConfig{
-    Workdir: ".hof/flow",
-  }, opts, popts))
+  //ctx.Use(info.NewBookkeeping(info.BookkeepingConfig{
+    //Workdir: ".hof/flow",
+  //}, opts, popts))
   ctx.Use(sync.NewPool(opts, popts))
 }
