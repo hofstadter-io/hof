f26ef7575c4f5aadefbb25cc3d906abd711b1ee0
Tony Worm <tony@hofstadter.io>
2022-04-03 02:54:21 -0400

    remove unused code
    Signed-off-by: Tony Worm <tony@hofstadter.io>

--- lib/labels/docs.go
+++
@@ -1,4 +0,0 @@
-// Should be able to query select by labels and then apply one more more lables
-// So basically "Get" with label changes, will need to expand to some CRUD ops
-// almost their own thing, but want some real flexibility here
-package labels
