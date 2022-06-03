164ddeba48e7a86a0fcfd44e519a409fb8031bfe
Tony Worm <tony@hofstadter.io>
2022-06-03 09:51:08 -0400

    remove completion hack for _ alias

--- cmd/hof/cmd/completions.go
+++ cmd/hof/cmd/completions.go
@@ -34,36 +34,12 @@ To configure your bash shell to load completions for each session add to your ba
 . <(hof completion bash)
 `
 
-var (
-	BashHack = `
-alias _="hof"
-if [[ $(type -t compopt) = "builtin" ]]; then
-    complete -o default -F __start_hof _
-else
-    complete -o default -o nospace -F __start_hof _
-fi
-`
-
-	FishHack = `
-alias _="hof"
-#compdef _=hof
-#compdef _hof _
-`
-
-	ZshHack = `
-alias _="hof"
-`
-)
-
 var BashCompletionCmd = &cobra.Command{
 	Use:   "bash",
 	Short: "Generate Bash completions",
 	Long:  BashCompletionLong,
 	Run: func(cmd *cobra.Command, args []string) {
 		RootCmd.GenBashCompletionV2(os.Stdout, false)
-
-		// alias hof to _
-		fmt.Println(BashHack)
 	},
 }
 
@@ -72,9 +48,6 @@ var ZshCompletionCmd = &cobra.Command{
 	Short: "Generate Zsh completions",
 	Long:  "Generate Zsh completions",
 	Run: func(cmd *cobra.Command, args []string) {
-		// alias hof to _
-		fmt.Println(ZshHack)
-
 		RootCmd.GenZshCompletion(os.Stdout)
 	},
 }
@@ -85,9 +58,6 @@ var FishCompletionCmd = &cobra.Command{
 	Long:  "Generate Fish completions",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		// alias hof to _
-		fmt.Println(FishHack)
-
 		RootCmd.GenFishCompletion(os.Stdout, true)
 	},
 }
@@ -99,7 +69,6 @@ var PowerShellCompletionCmd = &cobra.Command{
 	Long:    "Generate PowerShell completions",
 
 	Run: func(cmd *cobra.Command, args []string) {
-
 		RootCmd.GenPowerShellCompletion(os.Stdout)
 	},
 }
