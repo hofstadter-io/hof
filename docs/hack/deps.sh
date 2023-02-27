4dc0b86fffbabc56d42210488b2670f715643d24
Tony Worm <tony@hofstadter.io>
2023-02-26 20:35:10 -0500

    hof/docs: add cue to hack/deps.sh

--- docs/hack/deps.sh
+++ docs/hack/deps.sh
@@ -4,6 +4,7 @@ set -eou pipefail
 # setup the development workspace
 
 HUGO_VER="0.109.0"
+CUE_VER="v0.5.0-beta.5"
 
 npm install
 ln -s ../node_modules assets/node_modules
@@ -16,6 +17,10 @@ wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VER}/hugo_extend
 tar -xf hugo.tar.gz
 chmod +x hugo
 sudo mv hugo /usr/local/bin/hugo
+
+wget https://github.com/cue-lang/cue/releases/download/${CUE_VER}/cue_${CUE_VER}_linux_amd64.tar.gz -O cue.tar.gz
+tar -xf cue.tar.gz
+sudo mv cue /usr/local/bin/cue
 
 popd
 rm -rf tmp
