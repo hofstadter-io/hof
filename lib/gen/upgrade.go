2de1008721c7723c69d0b540498279aebe929bbb
Tony Worm <tony@hofstadter.io>
2023-04-26 12:49:41 -0400

    hof/gen: improve error message, add some notes

--- lib/gen/upgrade.go
+++ lib/gen/upgrade.go
@@ -13,6 +13,17 @@ import (
 const noRootFmt = `warning: root datamodel not found for child with %s at %s
 please pass the full datamodel to In values and select out later`
 
+// TODO, we may be able to move the upgrade logic to the datamodel package
+// and then unify the value with the ones we discover here.
+// The reason we are doing it here now is that the references get lost during decoding
+// and any changes we make to the datamodel are not reflected within the In value of the Genartor
+// Ideally, we could do this once and update values as needed.
+// It is likely we are doing work more than once.
+// We also need to ensure some order of operations here,
+//   as we need to have the history injected prior to decoding the majority of the Generator
+//   so that users can reference the history within the generator.
+//   The notable use case is making a file per snapshot for database migrations.
+
 func (G *Generator) upgradeDMs(dms []*datamodel.Datamodel) error {
 	if len(dms) == 0 {
 		return nil
@@ -30,7 +41,7 @@ func (G *Generator) upgradeDMs(dms []*datamodel.Datamodel) error {
 
 	// assert that there is only 1 gN
 	if len(gNs) != 1 {
-		return fmt.Errorf("%s at %s created multple $hof Nodes, you should not mix things like generators and datamodels withing the (exactly) same value, nesting is ok", G.Hof.Label, G.Hof.Path)
+		return fmt.Errorf("%s at %s created multiple $hof Nodes, you should not mix things like generators and datamodels withing the (exactly) same value, nesting is ok", G.Hof.Label, G.Hof.Path)
 	}
 
 	gN := gNs[0]
@@ -87,6 +98,10 @@ func (G *Generator) injectHistory(hn *hof.Node[any], dms []*datamodel.Datamodel,
 	}
 
 	// fmt.Println("found @history at: ", hn.Hof.Path)
+
+	// Instead of this check, we should try to walk the root to find where it aligns with the current hn.
+	// In this way, we can hopefully write code that is ignorant of where in the node tree it is.
+
 	if hn.Hof.Datamodel.Root {
 		hist := root.Node.T.History()
 		// fmt.Println("injecting hist at: ", hn.Hof.Path, len(hist), hist[0].Timestamp)
@@ -122,6 +137,9 @@ func (G *Generator) injectHistory(hn *hof.Node[any], dms []*datamodel.Datamodel,
 		// fmt.Println(G.CueValue)
 	} else {
 		if root == nil {
+			// The main problem here is knowing what datamodel a nested DM identirfier is part of.
+			// It is trivial to construct an example where the same name appears in two different datamodels.
+
 			fmt.Println("warning: root datamodel not found for child with history at, please pass the full datamodel to In values and select out later", hn.Hof.Path)
 		} else {
 			if G.Verbosity > 0 {
