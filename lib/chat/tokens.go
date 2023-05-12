504f98325428f879a420f144a6fe01702e505035
Tony Worm <tony@hofstadter.io>
2023-05-11 23:07:03 -0400

    add some deps & code for research

---
+++ lib/chat/tokens.go
@@ -0,0 +1,14 @@
+package chat
+
+import (
+	"github.com/pkoukk/tiktoken-go"
+)
+
+// https://github.com/pkoukk/tiktoken-go
+// https://pkg.go.dev/github.com/pkoukk/tiktoken-go
+
+// count tokens so we can inform users and better size our requests
+
+var _ = tiktoken.Tiktoken{}
+
+
