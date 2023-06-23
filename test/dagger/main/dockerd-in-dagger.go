20fbd5bdc9d596ea66aa6b6cd077fcbd544fc73f
Tony Worm <tony@hofstadter.io>
2023-06-23 14:15:41 -0400

    hof/test: dockerd-in-dagger working in cloud VM, needed mtu settings

--- test/dagger/main/dockerd-in-dagger.go
+++ test/dagger/main/dockerd-in-dagger.go
@@ -36,31 +36,34 @@ func main() {
 		client: client,
 	}
 
+	err = R.clientHack()
+	checkErr(err)
+	return
+
+	// dockerd / setup container with dind
+	daemon, err := R.daemonContainer()
+	checkErr(err)
+
+	_, err = daemon.ExitCode(R.ctx)
+	checkErr(err)
+
+
+	// dockerd / start in coroutine
+	go func() {
+		fmt.Println("starting daemon")
+	}()
+
+	// dockerd / wait for ready
+	fmt.Println("sleeping")
+	time.Sleep(10 * time.Second)
+
 	// docker-cli
 	base, err := R.baseContainer()
 	checkErr(err)
 
-	// docker daemon | dind
-	daemon, err := R.daemonContainer()
-	checkErr(err)
-
-	err = R.clientHack(daemon)
-	checkErr(err)
-
-	return
-
 	// attach deemon as a service to our base image
 	cntr, err := R.attachService(base, daemon)
 	checkErr(err)
-
-	go func() {
-		fmt.Println("starting daemon")
-		_, err = daemon.ExitCode(R.ctx)
-		checkErr(err)
-	}()
-
-	fmt.Println("sleeping")
-	time.Sleep(10*time.Second)
 
 	err = R.dockerInfo(cntr)
 	checkErr(err)
@@ -69,10 +72,70 @@ func main() {
 	checkErr(err)
 }
 
+func (R *runtime) clientHack() error {
+
+	t, err := R.baseContainer()
+	if err != nil {
+		return err
+	}
+
+	daemon, err := R.daemonContainer()
+	if err != nil {
+		return err
+	}
+
+	t = t.Pipeline("attach/daemon")
+	t, err = R.attachService(t, daemon)
+	if err != nil {
+		return err
+	}
+
+	t = t.WithEnvVariable("CACHE", time.Now().String())
+	// curl the registry
+	t = t.Pipeline("curl/registry")
+	t = t.WithExec([]string{"curl", "https://registry-1.docker.io"})
+	out, err := t.Stdout(R.ctx)
+	if err != nil {
+		return err
+	}
+	fmt.Println(out)
+
+	// curl the daemon
+	t = t.Pipeline("curl/daemon")
+	t = t.WithExec([]string{"curl", "global-dockerd:2375/info"})
+	out, err = t.Stdout(R.ctx)
+	if err != nil {
+		return err
+	}
+	fmt.Println(out)
+
+	// docker cli
+	t = t.Pipeline("docker/pull")
+	t = t.WithExec([]string{"docker", "pull", "hello-world"})
+	out, err = t.Stdout(R.ctx)
+	if err != nil {
+		return err
+	}
+	fmt.Println(out)
+
+	t = t.Pipeline("docker/run")
+	t = t.WithExec([]string{"docker", "run", "hello-world"})
+	out, err = t.Stdout(R.ctx)
+	if err != nil {
+		return err
+	}
+	fmt.Println(out)
+
+	// final output / force evaluation
+
+	return nil
+}
+
 func (R *runtime) baseContainer() (*dagger.Container, error) {
 
 	c := R.client.Container().From(dockerVer + "-cli")
 	c = c.Pipeline("base/image")
+	c = c.WithMountedCache("/tmp", R.client.CacheVolume("shared-tmp"))
 	c = c.WithExec([]string{"apk", "add", "curl"})
 
 	return c, nil
@@ -87,14 +150,19 @@ func (R *runtime) daemonContainer() (*dagger.Container, error) {
 	c = c.WithMountedCache("/var/lib/docker", R.client.CacheVolume("docker-cache"))
 	c = c.WithExposedPort(2375)
 
+	c = c.WithExec([]string{"apk", "add", "curl"})
+
+	c = c.WithEnvVariable("CACHE", time.Now().String())
+	c = c.WithExec([]string{"curl", "https://registry-1.docker.io"})
 	c = c.WithExec(
 		[]string{
 			"dockerd",
-			"--log-level=debug",
+			"--log-level=warn",
 			"--host=tcp://0.0.0.0:2375",
 			"--tls=false",
+			"--debug",
 		},
-		dagger.ContainerWithExecOpts{ InsecureRootCapabilities: true },
+		dagger.ContainerWithExecOpts{InsecureRootCapabilities: true},
 	)
 	return c, nil
 }
@@ -103,7 +171,6 @@ func (R *runtime) attachService(c, s *dagger.Container) (*dagger.Container, erro
 	t := c.Pipeline("docker/service")
 	t = t.WithEnvVariable("DOCKER_HOST", "tcp://global-dockerd:2375")
 	t = t.WithServiceBinding("global-dockerd", s)
-	t = t.WithMountedCache("/tmp", R.client.CacheVolume("shared-tmp"))
 
 	return t, nil
 }
@@ -119,26 +186,6 @@ func (R *runtime) dockerInfo(c *dagger.Container) error {
 	}
 
 	fmt.Println(out)
-	return nil
-}
-
-func (R *runtime) clientHack(daemon *dagger.Container) error {
-
-	cntr, err := R.baseContainer()
-	if err != nil {
-		return err
-	}
-
-	cntr, err = R.attachService(cntr, daemon)
-	if err != nil {
-		return err
-	}
-
-	path := "/info"
-	cntr = cntr.WithExec([]string{"curl", "global-dockerd:2375" + path})
-
-	cntr.Sync(R.ctx)
-
 	return nil
 }
 
