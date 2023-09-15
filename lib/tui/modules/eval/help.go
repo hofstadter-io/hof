be8caaa00aa3f212e59bb4e7542aa2ce7c79079f
Tony Worm <1390600+verdverm@users.noreply.github.com>
2023-09-15 07:50:24 -0400

    hof/tui: another round on the help text (#344)

--- lib/tui/modules/eval/help.go
+++ lib/tui/modules/eval/help.go
@@ -16,17 +16,13 @@ const EvalHelpText = `
   New releases will expand feature support,
   notably [violet]flow[-], [violet]gen[-], and [violet]vet[-] are upcoming.
 
-  [dodgerblue::bu]Contents:[darkgray::-]
-    Legend
-    App Controls
-    Panel Management
-    Navigation
-    Components 
-      Items
-      Commands
-      Dashbords
-    Examples
-    Getting Help
+[dodgerblue::bu]Contents:[darkgray::-]
+  App Controls
+  Overview 
+  Items
+  Commands
+  Dashbords
+  Getting Help
 [-]
 [dodgerblue::bu]Legend:[-::-]
   [lime]A[-] = ALT
@@ -46,34 +42,13 @@ const EvalHelpText = `
   [red]Ctrl-Alt-c[-]   close [gold::bi]_[ivory]Hofstadter[-::-] :[
    [red]:q[-]          (from the command box)
 
-[dodgerblue::bu]Panel Management:[-::-]
-  [lime]A-J[-]          create item before
-  [lime]A-K[-]          create item after
-  [lime]A-H[-]          move item before
-  [lime]A-L[-]          move item after
-  [lime]A-T[-]          split current item
-  [lime]A-D[-]          delete current item
-  [lime]A-F[-]          toggle flex direction
-  [lime]A-P[-]          toggle panel borders
-  [lime]A-O[-]          toggle item borders
 
-[dodgerblue::bu]Navigation:[-::-]
-  [lime]click[-]        to focus any box   [darkgray](with mouse)[-]
-  [lime]C-<left>[-]     focus item left    [darkgray](with arrows)[-]
-  [lime]C-<down>[-]     focus item down
-  [lime]C-<up>[-]       focus item up
-  [lime]C-<right>[-]    focus item right
-  [lime]A-h[-]          focus item left    [darkgray](vim style)[-]
-  [lime]A-j[-]          focus item down
-  [lime]A-k[-]          focus item up
-  [lime]A-l[-]          focus item right
-
-[dodgerblue::bu]Components:[-::-]
+[dodgerblue::bu]Overview:[-::-]
 
   The main component is a Dashboard with Panels.
   Panels contain items or other panels for layout.
   Items contain widgets with data sources and are
-  controlled through the command box ([lime]C-<space>[-]).
+  controlled through the command box ([lime]C-<space>[-],[lime]C-P[-],[lime]<esc>[-]).
 
   [darkgray]The [dodgerblue]Command Box[darkgray] will modify the [gold]Item[darkgray] currently or last focused.[-]
   [darkgray]There are modifiers to specifiy non-focused items, see below.[-]
@@ -85,24 +60,33 @@ const EvalHelpText = `
 
   Common commands, details below
 
-  [violet]play[-]         open a fresh [gold]Playground[-]
-  [violet]play (args)[-]  open a [gold]Playground[-] with data
-  [violet]view (args)[-]  open a [gold]Viewer[-] with data
-  [violet]conn (args)[-]  connect values to create a chain
-  [violet]help[-]         open these help contents
+  [violet]play         [-]open a fresh [gold]Playground[-]
+  [violet]play (args)  [-]open a [gold]Playground[-] with data
+  [violet]view (args)  [-]open a [gold]Viewer[-] with data
+  [violet]conn (args)  [-]connect values to create a chain
+  [violet]watch        [-]watch inputs and refresh widgets
+  [violet]help         [-]open these help contents
+
 
 [dodgerblue::bu]Items:[-::-]
 
-  [gold]Value View[-] allows you to explore CUE with control of options.
-  You will see many options at the top-middle, green is enabled
-  The represent various CUE evaluation states that you can play with
-  ** They toggle by using the letter without modifiers **
+  [gold]Viewer[-] allows you to explore CUE with control of options.
+  You will see many options at the top-middle, green is enabled.
+  These represent various CUE evaluation states that you can play with.
+  ** [::i]They toggle by using the letter without modifiers[-::-] **
 
+  [blue::bu]Hotkeys[-::-]
   [lime]TCJY[-]         Tree, Cue, Json, Yaml
   [lime]vcfr[-]         Validate, Concrete, Final, ResolveReferences
   [lime]ei[-]           Ignore Errors, Inline Output
   [lime]doh[-]          Definitions, Optional, Hidden
   [lime]DA[-]           Docs, Attributes
+
+  [blue::bu]Commands[-::-]
+  [violet]view [lightseagreen]<data>            [-]open a new [gold]Viewer[-] with [lightseagreen]<data-source>[-]
+  [violet]view [lightseagreen]test.cue          [-]create empty playG with scope from file
+  [violet]view [lightseagreen]./                [-]create empty playG with scope from dir
+  [violet]view [lightseagreen]<eval args>       [-]create empty playG with scope from eval args & flags
 
   [gold]Playground[-] is a multi-widget Item for working with CUE.
   You can edit CUE and see the results in real-time, with optional scope.
@@ -111,10 +95,25 @@ const EvalHelpText = `
     [darkgray]2. [gold]editor[-] for the [lightseagreen]main[-] value
     [darkgray]3. [gold]viewer[-] for the [lightseagreen]final[-] value
 
+  [blue::bu]Hotkeys[-::-]
   [lime]A-f[-]          [lightseagreen]Rotate[-] this item  (lowercase of Panel rotate hotkey)
   [lime]A-R[-]          [lightseagreen]Reload[-] data source and refresh
   [lime]A-s[-]          [lightseagreen]Toggle[-] toggle the scope viewer
   [lime]A-S[-]          [lightseagreen]Toggle[-] toggle the scope usage
+
+  [blue::bu]Commands[-::-]
+  [violet]play [lightseagreen]                              [-]create a new, empty playground
+  [violet]play [lightseagreen]test.cue                      [-]create play with content from a file
+  [violet]play [lightseagreen]bash kubectl ...              [-]create play with json output of a command
+  [violet]play [lightseagreen]https://cuelang.org/play...   [-]create play with content from a cue/play link
+  [violet]push [lightseagreen]                              [-]push the current edit text to cue/play, link returned
+  [violet]play scope [lightseagreen]test.cue                [-]create new play with scope from file
+  [violet]play scope [lightseagreen]./                      [-]create new play with scope from dir
+  [violet]play scope [lightseagreen]<eval args & flags>     [-]create new play with scope from eval args & flags
+  [violet]play scope [lightseagreen]<eval args & flags>     [-]create new play with scope from eval args & flags
+
+  There are many more commands for working with widgets, primarily [gold]play[-].
+  The [dodgerblue]Commands[-] section below has more examples and advanced options.
 
   [gold]Scope[-] is extra CUE and/or data that your edited CUE value
   will be evaluated with, as the scope or context. This is used to
@@ -123,65 +122,103 @@ const EvalHelpText = `
   The concept is tied to the CUE compiler and sets the lexical context for
   the CUE currently being processed. See https://pkg.go.dev/cuelang.org/go@v0.6.0/cue#Scope
 
+
 [dodgerblue::bu]Commands:[-::-]
 
   Items and widgets are controlled through the [dodgerblue]Command Box[-] ([lime]C-<space>[-]).
   First, make sure the item you want to change is focused (by [lime]clicking[-] on it).
+  You've see several above with the [gold]Items[-]. Some create a new [gold]Item[-]
+  while others modify the data or configuration of the current [gold]Item[-].
   The general format for commands is as follows.
 
-  [violet]<command> <args> [lightseagreen]<data source>[-]
+  [violet]<command> [lightseagreen]<args> <data source>[-]
 
-  [gold]Data Sources[-]
+  [blue::bu]Data Sources[-::-]
   [violet]<command> [lightseagreen]<eval args and flags>  [-](same as cue and hof)
   [violet]<command> [lightseagreen]https://...            [-](any http json response)
   [violet]<command> [lightseagreen]bash <args...>         [-](any bash json output)
-  [violet]<command> [lightseagreen]conn <path> <expr>     [-](dot path to item, optional expr)
+  [violet][lightseagreen]conn <path> <expr>               [-](dot path to item, optional expr)
 
-  [gold]Item Commands[-]         [darkgray](these create new widgets)[-]
-  [violet]<item>[-]       [lightseagreen]<data>[-]   same as [violet]<item> value[-]
-  [violet]<item> value[-] [lightseagreen]<data>[-]   set the main value
-  [violet]<item> scope[-] [lightseagreen]<data>[-]   set the scope value
-  [gold]items: [violet]play,view,help[-]
+  [blue::bu]Setting Sources[-::-]                   [darkgray](these modify the widget)[-]
+  (these are the same as item creation, but modify the current item)
+  [violet]set.value [lightseagreen]<data>                 [-]sets the value for the current play
+  [violet]set.scope [lightseagreen]<data>                 [-]sets the scope for the current play
+  [violet]conn [lightseagreen]<path> <expr>               [-]sets the scope for the current play
+                                    from the result of another play item
+  (path is an id/name dotpath to the source item, top-left of box)
+  (expr is a CUE dotpath to select a subvalue from final)
 
-  [gold]Update Commands[-]       [darkgray](these modify the widget)[-]
-  [violet]set.scope [lightseagreen]<data>[-]      set the scope for the current widget
-  [violet]set.value [lightseagreen]<data>[-]      set the value for the current widget
+  [blue::bu]Watching Inputs[-::-]                  [darkgray](so you can still edit files your IDE)[-]
+  [violet]refresh [lightseagreen]<delay>                  [-]change the default refresh time for play edit
+  [violet]watch [lightseagreen]                           [-]watch the current item with default delay
+  [violet]watch [lightseagreen]<delay>                    [-]watch the current item with custom delay
+  [violet]watchGlobs [lightseagreen]<globs>               [-]manually set watched globs that trigger
+  [violet]set.value.watch [lightseagreen]<delay>          [-]watch the current value with custom delay
+  [violet]set.scope.watch [lightseagreen]<delay>          [-]watch the current scope with custom delay
+  [violet]set.value.watchGlobs [lightseagreen]<globs>     [-]set the file flobs used for value watch
+  [violet]set.scope.watchGlobs [lightseagreen]<globs>     [-]set the file flobs used for scope watch
+  (delay is like 1s or 42ms, globs are like *.json, */*.json, or **/*)
+  (if watch seems stuck, refresh with [lime]A-R[-] or a dashboar save/load)
 
-  [gold]Other Commands[-]
-  [violet]push[-]                   send editor text to cuelang.org/play
-  [violet]write  <file>[-]          save editor text to file
-  [violet]export <file>[-]          save final value to file
-  [violet]set.panel.name <name>[-]  set the focused panel name
-  [violet]set.item.name  <name>[-]  set the focused item name
-  [violet]insert <loc>[-]           insert a new item at <loc>
-                         (head,tail,prev,next,index)
+  [blue::bu]Push and Save[-::-]
+  [violet]push                     [-]send editor text to cuelang.org/play
+  [violet]write  <file>            [-]save editor text to file
+  [violet]export <file>            [-]save final value to file
+
 
 [dodgerblue::bu]Dashboards:[-::-]
-  As you layout panels and items, set their data,
-  you will want to save, load, and share these.
 
+  Dashboards are sets of panels and items along with their
+  configuration and data sources. There are hotkeys and commands to
+  layout panels and items, as well as save, load, and share them.
+
+  [blue::bu]Panel Management[-::-]
+  [lime]A-J[-]          create a new item before
+  [lime]A-K[-]          create a new item after
+  [lime]A-H[-]          move (swap) item with prev
+  [lime]A-L[-]          move (swap) item with next
+  [lime]A-T[-]          split the current item
+  [lime]A-D[-]          delete the current item
+  [lime]A-F[-]          toggle flex direction
+  [lime]A-P[-]          toggle panel borders
+  [lime]A-O[-]          toggle item borders
+
+  [violet]insert <loc>[-]           insert a new item at <loc>
+  [violet]insert @<path> <loc>[-]   insert a new item, from @<path>, at <loc>
+    <loc>: {head,tail,prev,next,index}
+    @<path>: p0.b2.name style path to act on
+             Useful when a panel has only panels and you want an item
+  [violet]set.panel.name <name>    [-]set the focused panel name
+  [violet]set.item.name  <name>    [-]set the focused item name
+
+  [blue::bu]Navigation[-::-]
+  [lime]click[-]        to focus any box   [darkgray](with mouse)[-]
+  [lime]C-<left>[-]     focus item left    [darkgray](with arrows)[-]
+  [lime]C-<down>[-]     focus item down
+  [lime]C-<up>[-]       focus item up
+  [lime]C-<right>[-]    focus item right
+  [lime]A-h[-]          focus item left    [darkgray](vim style)[-]
+  [lime]A-j[-]          focus item down
+  [lime]A-k[-]          focus item up
+  [lime]A-l[-]          focus item right
+
+  [blue::bu]Saving and Loading[-::-]
   [violet]list[-]          list available dashboards
   [violet]save [lightseagreen]<dest>[-]   save the current view 
   [violet]show [lightseagreen]<dest>[-]   show the save file
   [violet]load [lightseagreen]<dest>[-]   load and replace current
 
-  [gold]Destinations[-]  ([violet]<command> [lightseagreen]<dest>[-])
-  [violet]<command> [lightseagreen]./<path>               [-](exact location)
+  [blue::bu]Destinations[-::-]
+  [violet]<command> [lightseagreen]./dir/file.cue         [-](exact location)
   [violet]<command> [lightseagreen]<name>                 [-](module location)
   [violet]<command> [lightseagreen]@<name>                [-](global, user cache dir)
   [violet]<command> [lightseagreen]https://...            [-](http get with ?id=...)
            (contents are CUE)      (post returns the id)
 
-[dodgerblue::bu]Examples:[-::-]
+  Note, that saving dashboards does not include the data.
+  The config and args are saved so that it can be recreated.
+  This means any files will have to be present when loaded.
 
-  [violet]view [lightseagreen]test.cue							        [-]create empty playG with scope from file
-  [violet]view [lightseagreen]./                            [-]create empty playG with scope from dir
-  [violet]view [lightseagreen]<eval args>                   [-]create empty playG with scope from eval args & flags
-  [violet]play [lightseagreen]                              [-]create a new, empty playground
-  [violet]play [lightseagreen]test.cue                      [-]create playG with content from a file
-  [violet]play scope [lightseagreen]test.cue                [-]create empty playG with scope from file
-  [violet]play scope [lightseagreen]./                      [-]create empty playG with scope from dir
-  [violet]play scope [lightseagreen]<eval args>             [-]create empty playG with scope from eval args & flags
 
 [dodgerblue::bu]Getting Help:[-::-]
 
@@ -190,7 +227,7 @@ const EvalHelpText = `
 
   [gold]Docs:      [deepskyblue]https://docs.hofstadter.io[-]
   [gold]GitHub:    [deepskyblue]https://github.com/hofstadter-io/hof[-]
-  [gold]Slack:     [deepskyblue]https://join.slack.com/t/hofstadter-io/shared_invite/zt-e5f90lmq-u695eJur0zE~AG~njNlT1A[-]
   [gold]Discord:   [deepskyblue]https://discord.gg/6vgbKvPs[-]
+  [gold]Slack:     [deepskyblue]https://join.slack.com/t/hofstadter-io/shared_invite/zt-e5f90lmq-u695eJur0zE~AG~njNlT1A[-]
 
 `
