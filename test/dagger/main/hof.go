3ac8bbd8210152d3812406d7a4e0191170e3403c
Tony Worm <tony@topcialsource.com>
2024-05-17 16:39:05 -0400

    mod/test: properly disable mod testing in dagger

--- test/dagger/main/hof.go
+++ test/dagger/main/hof.go
@@ -113,9 +113,9 @@ func main() {
 	tests["dm"] = func() error {
 		return R.TestDatamodel(tester, source)
 	}
-	// tests["mod"] = func() error {
-	// 	return  R.TestMod(tester, source)
-	// }
+	tests["mod"] = func() error {
+		return  R.TestMod(tester, source)
+	}
 	tests["fmt"] = func() error {
 		return R.TestCommandFmt(tester, source)
 	}
@@ -134,7 +134,7 @@ func main() {
 		"st",
 		"dm",
 		"flow",
-		"mod",
+		// "mod",
 		"fmt",
 		"cue",
 	}
