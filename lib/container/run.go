65ae36f2fd5a944b99a83440c29fe83af6bd1a46
verdverm <verdverm@gmail.com>
2026-01-05 21:29:07 -0800

    (veggie) fill out support for docker/nerdctl/podman run flags

--- lib/container/run.go
+++ lib/container/run.go
@@ -3,16 +3,29 @@ package container
 import (
 	"context"
 	"fmt"
-
-	"github.com/hofstadter-io/hof/lib/yagu"
 )
 
 type Params struct {
-	Name    Name
-	Env     []string
-	Replace bool
+	Name    Name `json:"name,omitempty"`
+	Replace bool `json:"replace,omitempty"`
 
-	// TODO, add more docker stuff here
+	Entrypoint    string   `json:"entrypoint,omitempty"`
+	Env           []string `json:"env,omitempty"`
+	Hostname      string   `json:"hostname,omitempty"`
+	Mount         []string `json:"mount,omitempty"`
+	Network       string   `json:"network,omitempty"`
+	NetworkAlias  []string `json:"network-alias,omitempty"`
+	NoHealthcheck bool     `json:"no-healthcheck,omitempty"`
+	Platform      string   `json:"platform,omitempty"`
+	Privileged    bool     `json:"privileged,omitempty"`
+	Publish       []string `json:"publish,omitempty"`
+	Pull          string   `json:"pull,omitempty"`
+	Restart       string   `json:"restart,omitempty"`
+	User          string   `json:"user,omitempty"`
+	Volume        []string `json:"volume,omitempty"`
+	Workdir       string   `json:"workdir,omitempty"`
+
+	Args []string `json:"args,omitempty"`
 }
 
 func (r runtime) Run(ctx context.Context, ref Ref, p *Params) error {
@@ -22,26 +35,74 @@ func (r runtime) Run(ctx context.Context, ref Ref, p *Params) error {
 		}
 	}
 
-	port, err := yagu.GetFreePort()
-	if err != nil {
-		return fmt.Errorf("while getting a free port: %w", err)
+	args := []string{"run", "--detach"}
+
+	if p.Name != "" {
+		args = append(args, "--name", string(p.Name))
 	}
 
-	// TODO, build up args better based on Params
-
-	args := []string{
-		"run",
-		"-p",
-		fmt.Sprintf("%d:3000", port),
-		"--detach",
-		"--name", string(p.Name),
+	if p.Entrypoint != "" {
+		args = append(args, "--entrypoint", p.Entrypoint)
 	}
 
 	for _, e := range p.Env {
-		args = append(args, []string{"--env", e}...)
+		args = append(args, "--env", e)
+	}
+
+	if p.Hostname != "" {
+		args = append(args, "--hostname", p.Hostname)
+	}
+
+	for _, m := range p.Mount {
+		args = append(args, "--mount", m)
+	}
+
+	if p.Network != "" {
+		args = append(args, "--network", p.Network)
+	}
+
+	for _, na := range p.NetworkAlias {
+		args = append(args, "--network-alias", na)
+	}
+
+	if p.NoHealthcheck {
+		args = append(args, "--no-healthcheck")
+	}
+
+	if p.Platform != "" {
+		args = append(args, "--platform", p.Platform)
+	}
+
+	if p.Privileged {
+		args = append(args, "--privileged")
+	}
+
+	for _, pblsh := range p.Publish {
+		args = append(args, "--publish", pblsh)
+	}
+
+	if p.Pull != "" {
+		args = append(args, "--pull", p.Pull)
+	}
+
+	if p.Restart != "" {
+		args = append(args, "--restart", p.Restart)
+	}
+
+	if p.User != "" {
+		args = append(args, "--user", p.User)
+	}
+
+	for _, v := range p.Volume {
+		args = append(args, "--volume", v)
+	}
+
+	if p.Workdir != "" {
+		args = append(args, "--workdir", p.Workdir)
 	}
 
 	args = append(args, string(ref))
+	args = append(args, p.Args...)
 
 	if _, err := r.exec(ctx, args...); err != nil {
 		return fmt.Errorf("exec: %w", err)
